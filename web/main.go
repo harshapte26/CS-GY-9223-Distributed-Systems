0bf8e59570d18bea49c0b877d51dabc84605b2a0
anubis-robot <anubis@anubis-lms.io>
2022-11-21 04:44:11 +0000

    Anubis Cloud IDE Autosave netid=mrc9419

--- web/main.go
+++ web/main.go
@@ -1,14 +1,16 @@
 package main
 
-import ("fmt"
-	"net/http")
+import (
+	"fmt"
+	"net/http"
+)
 
-func index_handler(w http.ResponseWriter, r *http.Request){
+func index_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Go world !</h1>")
 	fmt.Fprintf(w, "<p>Golang is a wonderful language</p>")
 }
 
-func about_handler(w http.ResponseWriter, r* http.Request){
+func about_handler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, `
 	// <h1>About Golang</h1>
 	// <p>Golang is an open source programming language which allows you to build simple, reliable, and efficient applications.</p>
@@ -16,7 +18,7 @@ func about_handler(w http.ResponseWriter, r* http.Request){
 	// http.FileServer(http.Dir("./front-end"))
 }
 
-func main(){
+func main() {
 	// http.HandleFunc("/", index_handler)
 	http.Handle("/", http.FileServer(http.Dir("./front-end")))
 	http.ListenAndServe(":8000", nil)
