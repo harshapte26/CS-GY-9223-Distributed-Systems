dfa5e5a7b0d0a2fc23a18417e32f8c4257d191d8
anubis-robot <anubis@anubis-lms.io>
2022-12-06 00:14:34 +0000

    Anubis Cloud IDE Autosave netid=ha2179

--- web/main.go
+++ web/main.go
@@ -8,16 +8,16 @@ import (
 func index_handler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "<h1>Go world !</h1>")
 	// fmt.Fprintf(w, "<p>Golang is a wonderful language</p>")
+	http.ServeFile(w, r, "./auth/signup.html")
 
-	
 }
 
 func about_handler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, `
-	// <h1>About Golang</h1>
-	// <p>Golang is an open source programming language which allows you to build simple, reliable, and efficient applications.</p>
-	// `)
-	// http.FileServer(http.Dir("./front-end"))
+	fmt.Fprintf(w, `
+	<h1>About Golang</h1>
+	<p>Golang is an open source programming language which allows you to build simple, reliable, and efficient applications.</p>
+	`)
+	http.FileServer(http.Dir("./front-end"))
 }
 
 func main() {
@@ -25,4 +25,3 @@ func main() {
 	// http.Handle("/", http.FileServer(http.Dir("./front-end")))
 	http.ListenAndServe(":8000", nil)
 }
-
