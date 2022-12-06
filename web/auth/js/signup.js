2733bc20d13ce3589d1f24663e34dfb2fad377b5
anubis-robot <anubis@anubis-lms.io>
2022-12-06 00:49:37 +0000

    Anubis Cloud IDE Autosave netid=ha2179

--- web/auth/js/signup.js
+++ web/auth/js/signup.js
@@ -1,44 +1,46 @@
 function registerUser() {
 
-    var email = document.getElementById("inputRegisterEmail").value;
-    var password = document.getElementById("inputRegisterPassword").value;
-  
-    if (!firstName) {
-        toastr.error("Please enter First Name!")
-        return
-    }
-    if (!lastName) {
-        toastr.error("Please enter Last Name!")
-        return
-    }
-    if (!email) {
-        toastr.error("Please enter Email!")
-        return
-    }
-    if (!password) {
-        toastr.error("Please enter Password!")
-        return
-    }
-  
-    var registerData = {
-        "firstName": firstName,
-        "lastName": lastName,
-        "email": email,
-        "password": password
-    }
-    $.ajax({
-        url: "http://localhost:9090/register/",
-        method: "POST",
-        data: JSON.stringify(registerData),
-        dataType: 'json',
-        contentType: "application/json",
-        success: function (data) {
-            if (data.Status == 200) {
-                toastr.success(data.Message);
-            } else {
-                toastr.error(data.Message)
-            }
-  
-        },
-    });
+  var firstName = document.getElementById("firstname").value;
+  var lastName = document.getElementById("lastname").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("pwd").value;
+
+  if (!firstName) {
+      toastr.error("Please enter First Name!")
+      return
   }
+  if (!lastName) {
+      toastr.error("Please enter Last Name!")
+      return
+  }
+  if (!email) {
+      toastr.error("Please enter Email!")
+      return
+  }
+  if (!password) {
+      toastr.error("Please enter Password!")
+      return
+  }
+
+  var registerData = {
+      "firstName": firstName,
+      "lastName": lastName,
+      "email": email,
+      "password": password
+  }
+  $.ajax({
+      url: "http://localhost:8000/register/",
+      method: "POST",
+      data: JSON.stringify(registerData),
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+          if (data.Status == 200) {
+              toastr.success(data.Message);
+          } else {
+              toastr.error(data.Message)
+          }
+
+      },
+  });
+}
