678bfd309b61115a40d170e68bb3def67f8dd2a7
anubis-robot <anubis@anubis-lms.io>
2022-12-06 00:00:35 +0000

    Anubis Cloud IDE Autosave netid=ha2179

--- web/main.go
+++ web/main.go
@@ -6,8 +6,10 @@ import (
 )
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Go world !</h1>")
-	fmt.Fprintf(w, "<p>Golang is a wonderful language</p>")
+	// fmt.Fprintf(w, "<h1>Go world !</h1>")
+	// fmt.Fprintf(w, "<p>Golang is a wonderful language</p>")
+
+	
 }
 
 func about_handler(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +23,6 @@ func about_handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", index_handler)
 	// http.Handle("/", http.FileServer(http.Dir("./front-end")))
-	http.Handle("/signup", http.)
 	http.ListenAndServe(":8000", nil)
 }
 
