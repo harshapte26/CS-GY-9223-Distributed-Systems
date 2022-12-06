6d6d6b78836732e3e3ecb9312764d7a130f41cba
anubis-robot <anubis@anubis-lms.io>
2022-12-06 21:53:54 +0000

    Anubis Cloud IDE Autosave netid=ha2179

--- web/main.go
+++ web/main.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 )
 
-func index_handler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "<h1>Go world !</h1>")
-	fmt.Fprintf(w, "<p>Golang is a wonderful language</p>")
-	// http.ServeFile(w, r, "./auth/signup.html")
-	// http.Handle("/", http.FileServer(http.Dir("./front-end")))
+// func index_handler(w http.ResponseWriter, r *http.Request) {
+// 	// fmt.Fprintf(w, "<h1>Go world !</h1>")
+// 	// fmt.Fprintf(w, "<p>Golang is a wonderful language</p>")
+// 	// http.ServeFile(w, r, ".//signup.html")
+// 	http.Handle("/", http.FileServer(http.Dir("./front-end")))
 
-}
+// }
 
 func signup_handler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "<h1>Go world !</h1>")
@@ -36,9 +36,9 @@ func about_handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", index_handler)
+	// http.HandleFunc("/", index_handler)
 	//http.handle
-	// http.Handle("/", http.FileServer(http.Dir("./front-end")))
+	http.Handle("/", http.FileServer(http.Dir("./front-end")))
 	http.HandleFunc("/register/", signup_handler)
 	http.HandleFunc("/login/", login_handler)
 	http.ListenAndServe(":9786", nil)
